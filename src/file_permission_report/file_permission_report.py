726a2a89cd49bf7f485bf2f821a0683009a9454d
azhgh22 <azhgh22@freeuni.edu.ge>
2025-07-16 17:44:43 +0400

    ignore

---
+++ src/file_permission_report/file_permission_report.py
@@ -0,0 +1,12 @@
+from src.file import File
+
+
+class FilePermissionReport:
+    def __init__(self) -> None:
+        self.odd_files = []
+
+    def identify_odd_files(self,files:list[File]):
+        pass
+
+    def make_report(self) -> None:
+        pass
